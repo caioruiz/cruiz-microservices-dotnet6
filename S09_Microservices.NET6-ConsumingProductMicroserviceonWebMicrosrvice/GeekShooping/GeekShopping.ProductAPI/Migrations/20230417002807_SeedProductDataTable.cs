fce8c5cc10faec6ebd6415bba7a6f300b1ef4b52
Caio <caio.ruiz@icloud.com>
2023-04-17 07:08:44 -0300

    Consuming Product Microservice on Web Microservice

---
+++ S09_Microservices.NET6-ConsumingProductMicroserviceonWebMicrosrvice/GeekShooping/GeekShopping.ProductAPI/Migrations/20230417002807_SeedProductDataTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 2L, "T-Shirt", "Camiseta geek", "https://static3.tcdn.com.br/img/img_prod/460977/camiseta_masculina_unissex_jogos_classicos_nintendo_video_game_arcade_donkey_kong_sonic_preta_ev_98685_1_ec026f63f3151195828a42c0f48b2e45.jpg", "Camiseta geek jogos", 69.9m },
+                    { 3L, "Bone", "Bone geek", "https://static3.tcdn.com.br/img/img_prod/460977/camiseta_masculina_unissex_jogos_classicos_nintendo_video_game_arcade_donkey_kong_sonic_preta_ev_98685_1_ec026f63f3151195828a42c0f48b2e45.jpg", "Bone Super Mario", 49.9m },
+                    { 4L, "Book", "Livro geek", "https://static3.tcdn.com.br/img/img_prod/460977/camiseta_masculina_unissex_jogos_classicos_nintendo_video_game_arcade_donkey_kong_sonic_preta_ev_98685_1_ec026f63f3151195828a42c0f48b2e45.jpg", "Livro Sonic", 59.9m },
+                    { 5L, "Action Figure", "Boneco geek", "https://static3.tcdn.com.br/img/img_prod/460977/camiseta_masculina_unissex_jogos_classicos_nintendo_video_game_arcade_donkey_kong_sonic_preta_ev_98685_1_ec026f63f3151195828a42c0f48b2e45.jpg", "Logo Nintendo", 69.9m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+        }
+    }
+}
---
+++ S09_Microservices.NET6-ConsumingProductMicroserviceonWebMicrosrvice/GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindAll();
+
+        Task<ProductVO> FindById(long Id);
+
+        Task<ProductVO> Create(ProductVO vo);
+
+        Task<ProductVO> Update(ProductVO vo);
+
+        Task<bool> Delete(long id);
+
+    }
+}
